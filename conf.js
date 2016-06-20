35a81647dc5bfb83b427dc09c5058cbffa2f4e09
aluzardo <adolfoluzardocabrera@gmail.com>
2016-06-20 14:20:43 +0200

    Add appium configuration

--- conf.js
+++ conf.js
@@ -11,16 +11,20 @@ exports.config = {
         require: 'features/step_definitions/*.step.js',
         format: "pretty"
     },
-    
+
     seleniumAddress: 'http://localhost:4444/wd/hub',
+    // TODO Find a way to mix browsers and appium devices
+    // seleniumAddress: 'http://localhost:4723/wd/hub',
     specs: ['features/*.feature'],
     multiCapabilities: [
         {
             browserName: 'firefox'
-        }, /* TODO Safari is randomly failing (necessary restart safari and selenium server)
+        },
+        /* TODO Safari is randomly failing (necessary restart safari and selenium server)
+         {
+         browserName: 'safari'
+         },*/
         {
-            browserName: 'safari'
-        },*/ {
             browserName: 'chrome'
         }, {
             browserName: 'chrome',
@@ -36,5 +40,38 @@ exports.config = {
             browserName: 'chrome',
             'deviceName': 'Samsung Galaxy S4'
         }
+        // TODO Find a way to mix browsers and appium devices
+        /*,  {
+         platformName: 'iOS',
+         platformVersion: '9.1',
+         browserName: '',
+         app: 'safari',
+         deviceName: 'Mavrix Test iPad Mini',
+         'appium-version': "1.5.3",
+         "forceIpad": true,
+
+
+         },
+         {
+         browserName: 'chrome',
+         'appium-version': '1.5.3',
+         platformName: 'Android',
+         platformVersion: '6.0.1',
+         deviceName: '0084db084cced0d1'
+         }
+         {
+         browserName: 'chrome',
+         'appium-version': '1.5.3',
+         platformName: 'Android',
+         platformVersion: '4.2.2',
+         deviceName: '30048664b980c100'
+         },
+         {
+         browserName: 'chrome',
+         'appium-version': '1.5.3',
+         platformName: 'Android',
+         platformVersion: '5.0.2',
+         deviceName: '33005bd56ac6c223'
+         }*/
     ]
 };
