332fe70f207347f7f020e348528d7573dc7a8b84
aluzardo <adolfoluzardocabrera@gmail.com>
2016-06-20 16:31:50 +0200

    Pretty format

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 // Step 3 protractor tutorial http://www.protractortest.org/#/tutorial#step-3-changing-the-configuration
 exports.config = {
     // set to "custom" instead of cucumber.
-    framework: 'pretty',
+    framework: 'custom',
 
     // path relative to the current config file
     frameworkPath: require.resolve('protractor-cucumber-framework'),
@@ -9,7 +9,7 @@ exports.config = {
     // relevant cucumber command line options
     cucumberOpts: {
         require: 'features/step_definitions/*.step.js',
-        format: "summary"
+        format: "pretty"
     },
 
     seleniumAddress: 'http://localhost:4444/wd/hub',
